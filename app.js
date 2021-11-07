c8716ff8a6ea0583964c1ea06289435a245081f0
krzysztof <krzszk96@gmail.com>
2021-11-07 22:40:43 +0100

    multiple choice changes

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var honeyModal = document.getElementById("honeyModal"); // Get the modal
+var btnh = document.getElementById("honeyBtn"); // Get the button that opens the modal
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+btnh.onclick = function() {
+  honeyModal.style.display = "block";
 }
-
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  honeyModal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+//strawberries
+var strawModal = document.getElementById("strawModal"); 
+var btns = document.getElementById("strawBtn"); 
+btns.onclick = function() {
+  strawModal.style.display = "block";
+}
+var span = document.getElementsByClassName("close")[1];
+span.onclick = function() {
+  strawModal.style.display = "none";
+}
+
+//plums
+var plumModal = document.getElementById("plumModal"); 
+var btnp = document.getElementById("plumBtn"); 
+btnp.onclick = function() {
+  plumModal.style.display = "block";
+}
+var span = document.getElementsByClassName("close")[2];
+span.onclick = function() {
+  plumModal.style.display = "none";
+}
+
+//cherries
+var CherryModal = document.getElementById("cherryModal"); 
+var btnc = document.getElementById("cherryBtn"); 
+btnc.onclick = function() {
+  CherryModal.style.display = "block";
+}
+var span = document.getElementsByClassName("close")[3];
+span.onclick = function() {
+  CherryModal.style.display = "none";
 }
